3001dfc4eabb30f2fab606135f3fb34bcef197c8
Samuel Braun <office.samigo.a@gmail.com>
2023-06-13 16:44:14 +0200

    Split apart dev and prod databases

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,15 +1,19 @@
 
 import { container } from '@sapphire/framework';
 import { ROLE } from './general';
-const { supabase } = container;
+const { supabase, devMode } = container;
 
+const TABLE = {
+    CLANS: devMode ? 'clans_dev' : 'clans',
+    MEMBERS: devMode ? 'members_dev' : 'members',
+};
 
 export async function deleteClan(guildId: string) {
-    return await supabase.from('clans').delete().eq('guild_id', guildId);
+    return await supabase.from(TABLE.CLANS).delete().eq('guild_id', guildId);
 }
 
 export async function getClan(guildId: string) {
-    const clans = await supabase.from('clans').select('*').eq('guild_id', guildId);
+    const clans = await supabase.from(TABLE.CLANS).select('*').eq('guild_id', guildId);
     if (clans.data == null || clans.data.length === 0) {
         return null;
     }
@@ -17,7 +21,7 @@ export async function getClan(guildId: string) {
 }
 
 export async function getMember(discord_id: string, guildId: string) {
-    const members = await supabase.from('members').select('*').eq('discord_id', discord_id).eq('guild_id', guildId);
+    const members = await supabase.from(TABLE.MEMBERS).select('*').eq('discord_id', discord_id).eq('guild_id', guildId);
     if (members.data == null || members.data.length === 0) {
         return null;
     }
@@ -25,15 +29,15 @@ export async function getMember(discord_id: string, guildId: string) {
 }
 
 export async function removeMember(discord_id: string, guildId: string) {
-    return await supabase.from('members').delete().eq('discord_id', discord_id).eq('guild_id', guildId);
+    return await supabase.from(TABLE.MEMBERS).delete().eq('discord_id', discord_id).eq('guild_id', guildId);
 }
 
 export async function addMember(discord_id: string, clanId: string, role: ROLE = ROLE.MEMBER) {
-    return await supabase.from('members').insert([{ discord_id, clan_id: clanId, role }]);
+    return await supabase.from(TABLE.MEMBERS).insert([{ discord_id, clan_id: clanId, role }]);
 }
 
 export async function createClan(guildId: string, clanName: string, clanTag: string, clanDescription: string, ownerId: string) {
-    return await supabase.from('clans').insert([
+    return await supabase.from(TABLE.CLANS).insert([
         {
             guild_id: guildId,
             name: clanName,
